737aadef50454923f01f4832fd8390f764b7b683
Ian <44680390+IanC04@users.noreply.github.com>
2026-02-18 14:52:57 -0600

    Revamp home page. Add CS tutorials page.

--- src/app.js
+++ src/app.js
@@ -1,2 +1,9 @@
-const title = document.title;
-console.log(`${title} page loaded.`);
+console.log(`${document.title} page loaded.`);
+document.body.insertAdjacentHTML('beforeend',
+    `
+    <footer style="margin-top: 3rem; text-align: center; font-size: 0.7rem; opacity: 0.7;">
+        <p>&copy; 2026 Ian Chen</p>
+        <p><a href="#">Back to top</a></p>
+    </footer>
+`);
+console.log(`Injected footer.`);
